4e454a1b8af08a2130abf2c79cb10818198d8120
Emerson <emersonmedina42@gmail.com>
2023-06-16 19:32:53 -0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+function scrollHeader() {
+  const header = document.getElementById("header");
+  //When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add("scroll-header"); else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+/*=============== SERVICES MODAL ===============*/
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+/* Link active work */
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== LIGHT DARK THEME ===============*/
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
