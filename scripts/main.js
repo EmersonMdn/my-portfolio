02c7b3455a41a6e4415b397d1637f3e20ce01933
Emerson <emersonmedina42@gmail.com>
2023-06-21 02:33:27 -0300

    secciones realizadas: project, contact, testimonials

--- scripts/main.js
+++ scripts/main.js
@@ -3,17 +3,74 @@
 function scrollHeader() {
   const header = document.getElementById("header");
   //When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-  if (this.scrollY >= 50) header.classList.add("scroll-header"); else header.classList.remove("scroll-header");
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
 
 /*=============== SERVICES MODAL ===============*/
 
+const modalViews = document.querySelectorAll(".services__modal");
+const modalBtns = document.querySelectorAll(".services__button");
+const modalClose = document.querySelectorAll(".services__modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalButton, i) => {
+  modalButton.addEventListener("click", () => {
+    modal(i);
+    console.log("active modal clicked");
+  });
+});
+
+modalClose.forEach((modalButton) => {
+  modalButton.addEventListener("click", () => {
+    modalViews.forEach((mv) => mv.classList.remove("active-modal"));
+  });
+});
+
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
+var mixer = mixitup(".work__container", {
+  selectors: {
+    target: ".work__card",
+  },
+  animation: {
+    duration: 400,
+  },
+});
 
 /* Link active work */
+const linkWork = document.querySelectorAll(".work__item");
+
+function activeWork() {
+  linkWork.forEach((link) => link.classList.remove("active-work"));
+  this.classList.add("active-work");
+}
+
+linkWork.forEach((link) => link.addEventListener("click", activeWork));
 
 /*=============== SWIPER TESTIMONIAL ===============*/
+
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  spaceBetween: 24,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+  },
+});
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
